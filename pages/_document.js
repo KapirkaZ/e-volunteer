f9ea79b67dc753b124d723e5bd63d780411491ba
KapirkaZ <68941822+KapirkaZ@users.noreply.github.com>
2023-02-17 17:35:48 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -28,13 +28,13 @@ class MyDocument extends Document {
       <Html lang={currentLocale}>
         <Head>
       
-      <!-- Google Tag Manager -->
+      {/* Google Tag Manager */}
       <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-KKD4W69');`}}></Script>
-<!-- End Google Tag Manager -->
+{/* End Google Tag Manager */}
 
           <meta name="theme-color" content="#fff" />
 
@@ -111,10 +111,10 @@ class MyDocument extends Document {
 
         <body>
             
-            <!-- Google Tag Manager (noscript) -->
+            {/* Google Tag Manager (noscript) */}
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KKD4W69"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+  {/* End Google Tag Manager (noscript) */}
   
           <Main />
 
